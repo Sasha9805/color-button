4fcfb92992cb065c4841f7a42f6d2c2c5cb40cb2
Sasha Pimenov <pimenovs33@gmail.com>
2024-09-25 19:33:09 +0300

    Added tests and code to checkbox flow

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,26 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+	const [disabled, setDisabled] = useState(false);
 	const [buttonColor, setButtonColor] = useState("red");
 	const nextColor = buttonColor === "red" ? "blue" : "red";
 	return (
 		<div>
 			<button
 				className={buttonColor}
+				disabled={disabled}
 				onClick={() => setButtonColor(nextColor)}
 			>
 				Change to {nextColor}
 			</button>
+			<br />
+			<input
+				type="checkbox"
+				id="disable-button-checkbox"
+				defaultChecked={disabled}
+				onChange={(e) => setDisabled(e.target.checked)}
+			/>
+			<label htmlFor="disable-button-checkbox">Disable button</label>
 		</div>
 	);
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -23,3 +23,21 @@ test("button click flow", () => {
 	// 	"background-color": "rgb(0, 0, 255)",
 	// });
 });
+
+test("checkbox flow", () => {
+	render(<App />);
+
+	const buttonElement = screen.getByRole("button", { name: /blue/i });
+	const checkboxElement = screen.getByRole("checkbox", {
+		name: /disable button/i,
+	});
+
+	expect(buttonElement).toBeEnabled();
+	expect(checkboxElement).not.toBeChecked();
+
+	fireEvent.click(checkboxElement);
+	expect(buttonElement).toBeDisabled();
+
+	fireEvent.click(checkboxElement);
+	expect(buttonElement).toBeEnabled();
+});
